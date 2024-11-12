701447319f719712ab96c28facaea9eecc14e5b8
minhhuy230301 <huyhoavang1231@gmail.com>
2024-11-12 11:04:30 +0700

    update app.py

--- src/app.py
+++ src/app.py
@@ -23,7 +23,7 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index9Update2.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
